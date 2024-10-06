536e22afeb0cf135f26b6329ac2abec8d7dbc79b
Hamid Azimi <hamidazimi.azp@gmail.com>
2024-10-06 13:47:54 +0330

    fix hover in header

--- src/components/modules/Header/Header.jsx
+++ src/components/modules/Header/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
           </PopoverPanel>
         </Popover>
         <Menu>
-          <MenuButton className=" w-10 h-10 rounded-full bg-gray-800 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
+          <MenuButton className="w-10 h-10 rounded-full bg-gray-800 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none hover:bg-gray-700 focus:bg-gray-700">
             <img
               src="./avatar.jpg"
               alt="avatar"
@@ -61,20 +61,20 @@ const Header = () => {
           <MenuItems
             transition
             anchor="bottom end"
-            className="w-52 origin-top-right rounded-xl border border-black/5 bg-white p-1 text-sm/6 text-black transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+            className="w-52 origin-top-right rounded-xl border border-black/5 bg-white p-1 text-sm/6 text-black transition duration-100 ease-out focus:outline-none"
           >
             <MenuItem>
-              <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10 data-[focus]:bg-gray-50">
+              <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-50">
                 Your Profile
               </button>
             </MenuItem>
             <MenuItem>
-              <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10 data-[focus]:bg-gray-50">
+              <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-50">
                 Settings
               </button>
             </MenuItem>
             <MenuItem>
-              <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10 data-[focus]:bg-gray-50">
+              <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-50">
                 Sign Out
               </button>
             </MenuItem>
