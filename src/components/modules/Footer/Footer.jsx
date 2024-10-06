4e94d9f2d9cca75e147ffee2fac429d99ece2dff
Hamid Azimi <hamidazimi.azp@gmail.com>
2024-10-06 14:25:23 +0330

    add footer section

---
+++ src/components/modules/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white h-16 px-4 flex justify-between items-center w-full">
+      <p className="text-sm text-gray-700 text-center  w-full">
+        © 2024 Dashboard. All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,17 +1,18 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "../modules/SideBar/SideBar";
 import Header from "../modules/Header/Header";
+import Footer from "../modules/Footer/Footer";
 
 const Layout = () => {
   return (
     <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
       <SideBar />
-      <div className="flex-1">
+      <div className="flex-1 overflow-y-scroll flex-col gap-5 ">
         <Header />
-        <main className="overflow-scroll h-full pb-16">
+        <main className="">
           <Outlet />
         </main>
-        <footer>Footer</footer>
+        <Footer />
       </div>
     </div>
   );
